fb99f8948c8c6b84385e7912b45fdfed8303227e
pengxuanHuang <pacers0406@gmail.com>
2025-06-07 21:31:03 +0200

    main update

--- main.py
+++ main.py
@@ -23,26 +23,26 @@ def main():
     logging.info("Retrieving data")
     t1 = time.perf_counter()
 
-    get_data_by_day(args, data_config)
-    # if args.model == "xgboost":
-    #     X_train, y_train, X_val, y_val, X_test, y_test = get_data_xgboost(args, data_config)
-    # else:
-    #     tr_data, val_data, te_data, tr_inds, val_inds, te_inds = get_data(args, data_config)
+    #get_data_by_day(args, data_config)
+    if args.model == "xgboost":
+        X_train, y_train, X_val, y_val, X_test, y_test = get_data_xgboost(args, data_config)
+    else:
+        tr_data, val_data, te_data, tr_inds, val_inds, te_inds = get_data(args, data_config)
 
 
-    # t2 = time.perf_counter()
-    # logging.info(f"Retrieved data in {t2-t1:.2f}s")
-    # if args.model == "xgboost":
-    #     train_xgboost(X_train, X_val, X_test,y_train, y_val, y_test, args, data_config)
-    # else:
-    #     if not args.inference:
-    #         #Training
-    #         logging.info(f"Running Training")
-    #         train_gnn(tr_data, val_data, te_data, tr_inds, val_inds, te_inds, args, data_config)
-    #     else:
-    #         #Inference
-    #         logging.info(f"Running Inference")
-    #         infer_gnn(tr_data, val_data, te_data, tr_inds, val_inds, te_inds, args, data_config)
+    t2 = time.perf_counter()
+    logging.info(f"Retrieved data in {t2-t1:.2f}s")
+    if args.model == "xgboost":
+        train_xgboost(X_train, X_val, X_test,y_train, y_val, y_test, args, data_config)
+    else:
+        if not args.inference:
+            #Training
+            logging.info(f"Running Training")
+            train_gnn(tr_data, val_data, te_data, tr_inds, val_inds, te_inds, args, data_config)
+        else:
+            #Inference
+            logging.info(f"Running Inference")
+            infer_gnn(tr_data, val_data, te_data, tr_inds, val_inds, te_inds, args, data_config)
 
 if __name__ == "__main__":
     main()
